5efe4040d1a99c104acea3a8b1f763fb749bd868
abdelp <juniorperezpy@gmail.com>
2020-03-16 14:14:19 -0300

    Add initial validations for my_any? method

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -43,6 +43,19 @@ RSpec.describe Enumerable do
     it { expect { |b| { a: 1, b: 2 }.my_each_with_index(&b) }.to yield_successive_args([[:a, 1], 0], [[:b, 2], 1]) }
   end
 
+  describe "#my_select" do
+    subject { array.my_select }
+    let(:array) { [1, 2, 3] }
+
+    it 'is expected to return an enumerator when no block is given' do
+      should be_kind_of(Enumerator)
+    end
+
+    it 'is expected to return an array withall of the elements matching the block condition' do
+      
+    end
+  end
+
   describe '#my_all?' do
     subject { array.my_all? }
     let(:array) { [1, 2, 3] }
@@ -74,6 +87,32 @@ RSpec.describe Enumerable do
 
     it 'is expected to be truthy when all the elements of the coleccion has case equality with the parameter given' do
       expect(array.my_all?(param)).to be_truthy
+    end
+  end
+
+  describe '#my_any?' do
+    subject { array.my_any? }
+    let(:array) { [1, false, nil] }
+    let(:param) { Integer }
+
+    it 'is expected to be truthy when at least one element in the collection is truthy and no block nor parameter is given' do
+      should be_truthy
+    end
+
+    it 'is expected to be truthy when at least one of the elements in the collection passes the block condition' do
+      expect(array.my_any? { |item| item == 1 }).to be_truthy
+    end
+
+    it 'is expected to be falsy on an empty collection' do
+      expect([].my_any?).to be_falsy
+    end
+
+    # expect to receive all items of the collection of the same type,
+    # receive an argument
+    # and be truthy
+
+    it 'is expected to be truthy when at least one element of the coleccion has case equality with the parameter given' do
+      expect(array.my_any?(param)).to be_truthy
     end
   end
 end
